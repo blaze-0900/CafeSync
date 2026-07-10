13fcbd54c6c3888435ccbf8a8c489e1465312766
blaze-0900 <itzblaze009@gmail.com>
2026-07-10 20:21:34 +0530

    Initial Spring Boot project

---
+++ src/main/java/com/cafesync/CafeSync/CafeSyncApplication.java
@@ -0,0 +1,13 @@
+package com.cafesync.CafeSync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeSyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cafesync/CafeSync/CafeSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cafesync.CafeSync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
