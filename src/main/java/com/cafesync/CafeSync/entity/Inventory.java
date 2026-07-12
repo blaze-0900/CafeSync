98ef658dbb3a8c0e072a5b71eb4e900817213e8c
blaze-0900 <itzblaze009@gmail.com>
2026-07-12 13:32:42 +0530

    Inventory Table Added

---
+++ src/main/java/com/cafesync/CafeSync/entity/Inventory.java
@@ -0,0 +1,89 @@
+package com.cafesync.CafeSync.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "inventory")
+public class Inventory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long inventoryId;
+
+    @ManyToOne
+    @JoinColumn(name = "franchise_id", nullable = false)
+    private Franchise franchise;
+
+    @ManyToOne
+    @JoinColumn(name = "ingredient_id", nullable = false)
+    private Ingredient ingredient;
+
+    @Column(nullable = false)
+    private Double availableQuantity;
+
+    @Column(nullable = false)
+    private Double minimumQuantity;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    public Inventory() {
+    }
+
+    public Long getInventoryId() {
+        return inventoryId;
+    }
+
+    public void setInventoryId(Long inventoryId) {
+        this.inventoryId = inventoryId;
+    }
+
+    public Franchise getFranchise() {
+        return franchise;
+    }
+
+    public void setFranchise(Franchise franchise) {
+        this.franchise = franchise;
+    }
+
+    public Ingredient getIngredient() {
+        return ingredient;
+    }
+
+    public void setIngredient(Ingredient ingredient) {
+        this.ingredient = ingredient;
+    }
+
+    public Double getAvailableQuantity() {
+        return availableQuantity;
+    }
+
+    public void setAvailableQuantity(Double availableQuantity) {
+        this.availableQuantity = availableQuantity;
+    }
+
+    public Double getMinimumQuantity() {
+        return minimumQuantity;
+    }
+
+    public void setMinimumQuantity(Double minimumQuantity) {
+        this.minimumQuantity = minimumQuantity;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+}
