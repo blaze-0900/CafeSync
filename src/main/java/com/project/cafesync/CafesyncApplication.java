2c1d0e72b53158b8fa1c4abf158c1ccb6804bdde
blaze-0900 <itzblaze009@gmail.com>
2026-07-21 12:57:48 +0530

    Working project

---
+++ src/main/java/com/project/cafesync/CafesyncApplication.java
@@ -0,0 +1,13 @@
+package com.project.cafesync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafesyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafesyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/cafesync/CafesyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.cafesync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafesyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
